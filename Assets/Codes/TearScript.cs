8d42c34cbf670a65b8eaf76e9ec6cc9027083b93
Devon <71911489+palicoCodes@users.noreply.github.com>
2020-10-14 14:21:39 -0700

    Added arrow and cooldown

--- Assets/Codes/TearScript.cs
+++ Assets/Codes/TearScript.cs
@@ -6,19 +6,30 @@ public class TearScript : MonoBehaviour
 {
     public GameObject[] ObjectsToSpawn;
     public PlayerMovement player;
+    public float tearCooldown = 30;
+    private float startingCounter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingCounter = 0;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("space"))
+        if (Input.GetKey("left") || Input.GetKey("down") || Input.GetKey("up") || Input.GetKey("right"))
         {
-            GameObject tear = GameObject.Instantiate(ObjectsToSpawn[0]);
-            tear.transform.position = player.transform.position;
+            if (startingCounter == 0)
+            {
+                startingCounter = tearCooldown;
+                GameObject tear = GameObject.Instantiate(ObjectsToSpawn[0]);
+                tear.transform.position = player.transform.position;
+            }
+            if (startingCounter > 0)
+            {
+                startingCounter--;
+            }
         }
     }
 }
